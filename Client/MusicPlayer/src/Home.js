866fdd5f82b4f3deb053ba406cc556b2ffe8399d
Shaariq <mshaariq.p@gmail.com>
2022-09-02 14:35:20 +0200

    Autoplay on select

--- Client/MusicPlayer/src/Home.js
+++ Client/MusicPlayer/src/Home.js
@@ -17,16 +17,74 @@ import {
 
 const songs = [
   {
-    id: 1,
+    id: 0,
     artist: 'Gorillaz',
     url: require('./Assets/TestMusic/song1.mp3'),
-    title: 'Gorillaz - Feel Good Inc',
+    title: 'Feel Good Inc.',
+    albumArt: require('./Assets/Images/Feel_Good_Inc.jpeg'),
+  },
+  {
+    id: 1,
+    artist: 'Rick Astley',
+    url: require('./Assets/TestMusic/song2.mp3'),
+    title: 'Never Gonna Give You Up',
+    albumArt: require('./Assets/Images/Rickroll.jpeg'),
   },
   {
     id: 2,
-    artist: 'Rick Astley',
-    url: require('./Assets/TestMusic/song2.mp3'),
-    title: 'Rick Astley - Never Gonna Give You Up',
+    artist: 'Undertale',
+    url: require('./Assets/TestMusic/Megalovania.mp3'),
+    title: 'Megalovania',
+    albumArt: require('./Assets/Images/Megalovania.jpeg'),
+  },
+  {
+    id: 3,
+    artist: 'Darude',
+    url: require('./Assets/TestMusic/Sandstorm.mp3'),
+    title: 'Sandstorm',
+    albumArt: require('./Assets/Images/Sandstorm.png'),
+  },
+  {
+    id: 4,
+    artist: 'Smash Mouth',
+    url: require('./Assets/TestMusic/All_Star.mp3'),
+    title: 'All Star',
+    albumArt: require('./Assets/Images/All_Star.jpeg'),
+  },
+  {
+    id: 5,
+    artist: 'Foster The People',
+    url: require('./Assets/TestMusic/Pumped_up_Kicks.mp3'),
+    title: 'Pumped Up Kicks',
+    albumArt: require('./Assets/Images/Pumped_Up_Kicks.jpeg'),
+  },
+  {
+    id: 6,
+    artist: 'a-ha',
+    url: require('./Assets/TestMusic/Take_On_Me.mp3'),
+    title: 'Take On Me',
+    albumArt: require('./Assets/Images/Take_On_Me.jpeg'),
+  },
+  {
+    id: 7,
+    artist: 'Yes',
+    url: require('./Assets/TestMusic/Roundabout.mp3'),
+    title: 'Roundabout',
+    albumArt: require('./Assets/Images/Roundabout.jpeg'),
+  },
+  {
+    id: 8,
+    artist: 'Getter Jaani',
+    url: require('./Assets/TestMusic/Rockefeller_Street.mp3'),
+    title: 'Rockefeller Street (Nightcore)',
+    albumArt: require('./Assets/Images/Rockefeller_Street.jpeg'),
+  },
+  {
+    id: 9,
+    artist: 'Caramella Girls',
+    url: require('./Assets/TestMusic/Caramelldansen.mp3'),
+    title: 'Caramelldansen',
+    albumArt: require('./Assets/Images/Caramelldansen.jpeg'),
   },
 ];
 
@@ -49,14 +107,20 @@ const togglePlayback = async () => {
   }
 }
 
-const Home = () => {
+const Home = ({route, navigation}) => {
+  const {songIndex} = route.params;
+  const [trackIndex, setTrackIndex] = useState(1);
   const playbackState = usePlaybackState();
 
   useEffect(() => {
-    setupPlayer();
+    setupPlayer().then(() => {
+      TrackPlayer.skip(songIndex);
+      TrackPlayer.play();
+    })
+    console.log(songIndex);
 
-    // return () => TrackPlayer.destroy();
-  });
+    return () => TrackPlayer.destroy();
+  }, []);
 
   return (
     <View
@@ -65,7 +129,7 @@ const Home = () => {
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-      <TouchableOpacity onPress={() => TrackPlayer.play()}>
+      <TouchableOpacity onPress={() => {TrackPlayer.play()}}>
         <Text>Play</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => TrackPlayer.pause()}>
--- Client/MusicPlayer/src/Library.js
+++ Client/MusicPlayer/src/Library.js
@@ -13,70 +13,70 @@ import {
 
 const songs = [
   {
-    id: 1,
+    id: 0,
     artist: 'Gorillaz',
     url: require('./Assets/TestMusic/song1.mp3'),
     title: 'Feel Good Inc.',
     albumArt: require('./Assets/Images/Feel_Good_Inc.jpeg'),
   },
   {
-    id: 2,
+    id: 1,
     artist: 'Rick Astley',
     url: require('./Assets/TestMusic/song2.mp3'),
     title: 'Never Gonna Give You Up',
     albumArt: require('./Assets/Images/Rickroll.jpeg'),
   },
   {
-    id: 3,
+    id: 2,
     artist: 'Undertale',
     url: require('./Assets/TestMusic/Megalovania.mp3'),
     title: 'Megalovania',
     albumArt: require('./Assets/Images/Megalovania.jpeg'),
   },
   {
-    id: 4,
+    id: 3,
     artist: 'Darude',
     url: require('./Assets/TestMusic/Sandstorm.mp3'),
     title: 'Sandstorm',
     albumArt: require('./Assets/Images/Sandstorm.png'),
   },
   {
-    id: 5,
+    id: 4,
     artist: 'Smash Mouth',
     url: require('./Assets/TestMusic/All_Star.mp3'),
     title: 'All Star',
     albumArt: require('./Assets/Images/All_Star.jpeg'),
   },
   {
-    id: 6,
+    id: 5,
     artist: 'Foster The People',
     url: require('./Assets/TestMusic/Pumped_up_Kicks.mp3'),
     title: 'Pumped Up Kicks',
     albumArt: require('./Assets/Images/Pumped_Up_Kicks.jpeg'),
   },
   {
-    id: 7,
+    id: 6,
     artist: 'a-ha',
     url: require('./Assets/TestMusic/Take_On_Me.mp3'),
     title: 'Take On Me',
     albumArt: require('./Assets/Images/Take_On_Me.jpeg'),
   },
   {
-    id: 8,
+    id: 7,
     artist: 'Yes',
     url: require('./Assets/TestMusic/Roundabout.mp3'),
     title: 'Roundabout',
     albumArt: require('./Assets/Images/Roundabout.jpeg'),
   },
   {
-    id: 9,
+    id: 8,
     artist: 'Getter Jaani',
     url: require('./Assets/TestMusic/Rockefeller_Street.mp3'),
     title: 'Rockefeller Street (Nightcore)',
     albumArt: require('./Assets/Images/Rockefeller_Street.jpeg'),
   },
   {
-    id: 10,
+    id: 9,
     artist: 'Caramella Girls',
     url: require('./Assets/TestMusic/Caramelldansen.mp3'),
     title: 'Caramelldansen',
@@ -84,7 +84,7 @@ const songs = [
   },
 ];
 
-const Home = ({navigation}) => {
+const Home = ({navigation, route}) => {
   return (
     <SafeAreaView style={styles.body}>
       <FlatList
@@ -100,7 +100,7 @@ const Home = ({navigation}) => {
           <View style={styles.song}>
             <TouchableOpacity
               onPress={() => {
-                navigation.navigate('Home');
+                navigation.navigate('Home', {songIndex: item.id});
               }}>
               <View style={styles.songDetails}>
                 <View>
