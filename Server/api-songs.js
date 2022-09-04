55af6a273e002a7dd098a95e475ed7c0c0a316f1
Woeful_Wolf <54476280+WoefulWolf@users.noreply.github.com>
2022-09-04 15:22:06 +0200

    Create api-songs.js

---
+++ Server/api-songs.js
@@ -0,0 +1,15 @@
+const database = require("./database");
+
+class Songs {
+    AddSong(artist, name, albumn) {
+        // Add a song to the database
+        const text = "INSERT INTO songs(artist, name, albumn) VALUES($1, $2, $3)";
+        const values = [artist, name, albumn];
+        const res = database.Query(text, values);
+        return res;
+    }
+}
+
+// Create and export the accounts object
+let songs = new Songs();
+module.exports = songs;
