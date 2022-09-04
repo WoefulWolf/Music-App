2468adfa66065b7f5ab8567c7e344ade40f26f0b
Woeful_Wolf <54476280+WoefulWolf@users.noreply.github.com>
2022-09-04 15:22:01 +0200

    Create api-accounts.js

---
+++ Server/api-accounts.js
@@ -0,0 +1,58 @@
+const database = require("./database");
+const isValid = require("./validation");
+
+class Accounts {
+    async RegisterAccount(ID, email, username) {
+        // Check fields for validity
+        // Check ID
+        if (!isValid.String(ID)) {
+            return {
+                status: 400,
+                body: {"Invalid ID": "The ID '" + ID + "' is not valid"},
+            };
+        }
+
+        // Check email
+        if (!isValid.Email(email)) {
+            return {
+                status: 400,
+                body: {"Invalid email": "The email '" + email + "' is not valid"},
+            };
+        }
+
+        // Check username
+        if (!isValid.String(username)) {
+            return {
+                status: 400,
+                body: {"Invalid username": "The username '" + username + "' is not valid"},
+            };
+        }
+
+        // Create a new account in the database
+        const text = "INSERT INTO accounts VALUES($1, $2, $3, CURRENT_DATE)";
+        const values = [ID, email, username];
+        const res = await database.Query(text, values);
+        return res;
+    }
+
+    async GetAccount(ID) {
+        // Check fields for validity
+        // Check ID
+        if (!isValid.String(ID)) {
+            return {
+                status: 400,
+                body: {"Invalid ID": "The ID '" + ID + "' is not valid"},
+            };
+        }
+
+        // Get an account from the database
+        const text = "SELECT * FROM accounts WHERE \"User_ID\" = $1";
+        const values = [ID];
+        const res = await database.Query(text, values);
+        return res;
+    }
+}
+
+// Create and export the accounts object
+let accounts = new Accounts();
+module.exports = accounts;
