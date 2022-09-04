5ed0a71c79c0682463d96aef16c204d0eaacf201
Woeful_Wolf <54476280+WoefulWolf@users.noreply.github.com>
2022-09-04 15:21:56 +0200

    Create validation.js

---
+++ Server/validation.js
@@ -0,0 +1,14 @@
+class IsValid {
+    String(val) {
+        return (typeof val === 'string');
+    }
+
+    Email(val) {
+        if (!this.String(val)) return false;
+
+        return val.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/);
+    }
+}
+
+let isValid = new IsValid();
+module.exports = isValid;
