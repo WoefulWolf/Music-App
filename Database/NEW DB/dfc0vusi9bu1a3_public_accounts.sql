e2d056bd01cd1d64bfe2e7c9c86c6c3dd6feeb48
ZidanCS <1914752@students.wits.ac.za>
2022-10-27 08:34:27 +0200

    Database Updates
    Changes to database

--- Database/NEW DB/dfc0vusi9bu1a3_public_accounts.sql
+++ Database/NEW DB/dfc0vusi9bu1a3_public_accounts.sql
@@ -1,3 +1,4 @@
-insert into public.accounts (Email, Username, User_ID, Date_Joined) values ('test@test.com', 'test', '1234567890', '2022-10-05');
-insert into public.accounts (Email, Username, User_ID, Date_Joined) values ('2344635@students.wits.ac.za', '2344635@students.wits.ac.za', 'auth0|633dbaf39202a9200c09afc6', '2022-10-06');
-insert into public.accounts (Email, Username, User_ID, Date_Joined) values ('mshaariq.p@gmail.com', 'mshaariq.p@gmail.com', 'auth0|633db8f1d48027ee0bb736a1', '2022-10-10');
+UPDATE public.accounts SET "Email" = 'test@test.com', "Username" = 'test', "Date_Joined" = '2022-10-05' WHERE "User_ID" = '1234567890';
+UPDATE public.accounts SET "Email" = '2344635@students.wits.ac.za', "Username" = '2344635@students.wits.ac.za', "Date_Joined" = '2022-10-06' WHERE "User_ID" = 'auth0|633dbaf39202a9200c09afc6';
+UPDATE public.accounts SET "Email" = 'mshaariq.p@gmail.com', "Username" = 'mshaariq.p@gmail.com', "Date_Joined" = '2022-10-10' WHERE "User_ID" = 'auth0|633db8f1d48027ee0bb736a1';
+UPDATE public.accounts SET "Email" = 'cpvdm10@gmail.com', "Username" = 'cpvdm10@gmail.com', "Date_Joined" = '2022-10-12' WHERE "User_ID" = 'auth0|63464fca6bfc2845ff38e5e7';
